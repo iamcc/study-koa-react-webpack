33af3800b0ac06472ea37ef4aa05da72a545fa5a
CC <sic.hcq@gmail.com>
2015-08-14 16:40:50 +0800

    update

--- section-2/client/pages/Login.jsx
+++ section-2/client/pages/Login.jsx
@@ -2,7 +2,7 @@
 * @Author: CC
 * @Date:   2015-08-11 18:05:12
 * @Last Modified by:   CC
-* @Last Modified time: 2015-08-13 16:25:22
+* @Last Modified time: 2015-08-14 16:40:03
 */
 
 import React from 'react/addons'
@@ -30,10 +30,7 @@ export default class Login extends React.Component {
       loading: false,
       username: '',
       password: '',
-      error: {
-        username: '',
-        password: '',
-      }
+      errors: {}
     }
   }
 
@@ -41,7 +38,7 @@ export default class Login extends React.Component {
     if (auth.loggedIn) return router.transitionTo('app')
 
     for(let k in auth.error) {
-      this.state.error[k] = auth.error[k]
+      this.state.errors[k] = auth.error[k]
       this.form[k].focus()
     }
 
@@ -67,29 +64,28 @@ export default class Login extends React.Component {
 
     if (this.state.loading) return
 
-    this.state.error.username = false
-    this.state.error.password = false
+    const state = this.state
+    const errors = state.errors = {}
 
-    !this.state.username && (this.state.error.username = '用户名不能空')
-    !this.state.password && (this.state.error.password = '密码不能空')
-    this.state.password.length < 6 && (this.state.error.password = '密码不能少于6位数')
+    !state.username && (errors.username = '用户名不能空')
+    !state.password && (errors.password = '密码不能空')
+    state.password.length < 6 && (errors.password = '密码不能少于6位数')
 
-    !!this.state.error.password && this.form.password.focus()
-    !!this.state.error.username && this.form.username.focus()
+    !!errors.password && this.form.password.focus()
+    !!errors.username && this.form.username.focus()
 
-    if (this.state.error.username
-      || this.state.error.password) return this.setState(this.state)
+    if (errors.username || errors.password) return this.setState(state)
 
     this.setState({ loading: true })
 
-    AuthService.login(this.state.username, this.state.password, (e, res) => {
+    AuthService.login(state.username, state.password, (e, res) => {
       this.setState({ loading: false })
 
       if (res.ok) return AuthAction.login(res.body)
 
       if (e.status === 400) {
         for (let k in res.body) {
-          this.setState({ error: { [k]: res.body[k] } })
+          this.setState({ errors: { [k]: res.body[k] } })
           this.form[k].focus()
         }
         return
@@ -100,20 +96,22 @@ export default class Login extends React.Component {
   }
 
   render() {
+    const errors = this.state.errors
+
     return (
       <form className="ant-form-horizontal" style={this.state.style.form} onSubmit={this.handleSubmit.bind(this)}>
-        <div className={'ant-form-item' + (this.state.error.username ? ' has-error' : '')}>
+        <div className={'ant-form-item' + (errors.username ? ' has-error' : '')}>
           <label htmlFor="" className="col-6" required>Username: </label>
           <div className="col-18">
             <input type="text" className="ant-input" ref="username" valueLink={this.linkState('username')}/>
-            <p className="ant-form-explain">{this.state.error.username}</p>
+            <p className="ant-form-explain">{errors.username}</p>
           </div>
         </div>
-        <div className={'ant-form-item' + (this.state.error.password ? ' has-error' : '')}>
+        <div className={'ant-form-item' + (errors.password ? ' has-error' : '')}>
           <label htmlFor="" className="col-6" required>Password: </label>
           <div className="col-18">
             <input type="password" className="ant-input" ref="password" valueLink={this.linkState('password')}/>
-            <p className="ant-form-explain">{this.state.error.password}</p>
+            <p className="ant-form-explain">{errors.password}</p>
           </div>
         </div>
         <div className="ant-form-item">
